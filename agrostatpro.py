2d1bb3be2994968bbd5d560bc4dc7a0d01884095
Rafael Novais de Miranda <56369172+rafaelnm90@users.noreply.github.com>
2026-02-17 00:29:13 -0300

    Update agrostatpro.py

--- agrostatpro.py
+++ agrostatpro.py
@@ -3541,7 +3541,7 @@ st.markdown(
     <div style="text-align: center; color: #666; font-size: 0.9em; margin-top: 20px;">
         <hr style="margin-bottom: 15px; border-top: 1px solid #eee;">
         Developed by <b>Rafael Novais de Miranda</b><br>
-        📧 rafaelnovaismiranda@gmail.com | 📱 +55 (34) 9.99777-9966
+        📧 rafaelnovaismiranda@gmail.com
     </div>
     """,
     unsafe_allow_html=True
